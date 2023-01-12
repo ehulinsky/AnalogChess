665b30d44701d8e951bcaf09ec6e5b7077a6fa49
Ethan Hulinsky <ethan.hulinsky@gmail.com>
2023-01-11 20:56:23 -0600

    added castling and fixed some movement bugs

--- analogchessV4.py
+++ analogchessV4.py
@@ -147,7 +147,6 @@ class Piece():
             self.y = self.start_y
 
     def confirm(self,pieces):
-        global whites_turn
         
         if self.grabbed:
             self.grabbed=False
@@ -161,7 +160,6 @@ class Piece():
             self.start_x = self.x
             self.start_y = self.y
             self.turn += 1
-            whites_turn=not whites_turn
         
     def ungrab(self,pieces):
         if self.grabbed:
@@ -180,6 +178,15 @@ class Piece():
                     if event.type == pygame.QUIT:
                         pygame.quit()
                         quit()
+                    if event.type == pygame.MOUSEBUTTONUP:
+                        if dist(to_game_coords(pygame.mouse.get_pos()),(self.x,self.y)) < self.radius:
+                            self.confirm(pieces)
+                            return
+                        else:
+                            self.cancel(pieces)
+                            return
+                            
+                        
                     elif event.type == pygame.KEYDOWN:
                         if event.key == pygame.K_RETURN:
                             self.confirm(pieces)
@@ -397,7 +404,7 @@ class Pawn(Piece):
         
     def drag(self,new_p,pieces):
         if self.grabbed:
-
+            self.slide(0,0,pieces)
             if self.color == white:
                 path,dist=self.select_path((self.start_x,self.start_y),[[1,1],[-1,1],[0,1]],new_p)
                 path_len=math.sqrt(path[0]**2+path[1]**2)
@@ -495,6 +502,7 @@ class Rook(Piece):
         
     def drag(self,new_p,pieces):
         if self.grabbed:
+            self.slide(0,0,pieces)
             path,dist=self.select_path((self.start_x,self.start_y),[[1,0],[0,1]],new_p)
             path_len=math.sqrt(path[0]**2+path[1]**2)
             self.slide(path[0]*dist/path_len,path[1]*dist/path_len,pieces)
@@ -733,7 +741,99 @@ class Knight(Piece):
             '''
             
     def draw_paths(self,pieces):
-        pygame.draw.circle(see_through, RED_HIGHLIGHT, to_screen_coords((self.start_x,self.start_y)), (math.sqrt(5)+self.radius)/8*640,width=round(self.radius*640/8*2))
+        edge_positions = []
+        Radius=math.sqrt(5)
+        pieces_in_range_angles = []
+        for piece in pieces:
+            d=dist((self.start_x,self.start_y),(piece.x,piece.y))
+            if d<Radius+2*self.radius and d>Radius-2*self.radius and piece!=self and piece.color==self.color:
+                
+                #use law of cosines to find the angle that put the knight on the edge of the piece
+                cos_angle=((2*self.radius)**2-Radius**2-d**2)/(-2*Radius*d)
+                if cos_angle<=1:
+                    theta=math.acos(cos_angle)
+                    angle=math.radians(90)-math.atan2(piece.y-self.start_y,piece.x-self.start_x)
+                    if angle<0:
+                        angle=2*math.pi+angle
+                    pieces_in_range_angles.append(angle)
+                    piece_angle = math.atan2(piece.y-self.start_y,piece.x-self.start_x)
+                    edge_positions.append((self.start_x+Radius*math.cos(piece_angle+theta+0.001),self.start_y+Radius*math.sin(piece_angle+theta+0.001)))
+                    edge_positions.append((self.start_x+Radius*math.cos(piece_angle-theta-0.001),self.start_y+Radius*math.sin(piece_angle-theta-0.001)))
+
+
+        if self.start_x-Radius<self.radius:
+            edge_positions.append((self.radius,self.start_y+math.sqrt(Radius**2-(self.start_x-self.radius)**2)))
+            edge_positions.append((self.radius,self.start_y-math.sqrt(Radius**2-(self.start_x-self.radius)**2)))
+            #this is so that um uhhh uh dont worry about it :)
+            pieces_in_range_angles.append(1.5*math.pi)
+
+        if self.start_x+Radius>8-self.radius:
+            edge_positions.append((8-self.radius,self.start_y+math.sqrt(Radius**2-(8-self.start_x-self.radius)**2)))
+            edge_positions.append((8-self.radius,self.start_y-math.sqrt(Radius**2-(8-self.start_x-self.radius)**2)))
+            pieces_in_range_angles.append(0.5*math.pi)
+
+        if self.start_y-Radius<self.radius:
+            edge_positions.append((self.start_x+math.sqrt(Radius**2-(self.start_y-self.radius)**2),self.radius))
+            edge_positions.append((self.start_x-math.sqrt(Radius**2-(self.start_y-self.radius)**2),self.radius))
+            pieces_in_range_angles.append(math.pi)
+        
+        if self.start_y+Radius>8-self.radius:
+            edge_positions.append((self.start_x+math.sqrt(Radius**2-(8-self.start_y-self.radius)**2),8-self.radius))
+            edge_positions.append((self.start_x-math.sqrt(Radius**2-(8-self.start_y-self.radius)**2),8-self.radius))
+            pieces_in_range_angles.append(0)
+        
+        valid_edge_positions = []
+        #remove edge positions that place the knight off the board
+        for pos in edge_positions:
+            x=pos[0]
+            y=pos[1]
+            if x+self.radius<=8 and x-self.radius>=0 and y+self.radius<=8 and y-self.radius>=0:
+                valid=True
+                #and edge positions that are overlapping a piece
+                for piece in pieces:
+                    if piece.color == self.color:
+                        if piece.overlaps(Knight(x,y,self.color)) and piece!=self:
+                            valid=False
+                if valid:     
+                    valid_edge_positions.append(pos)
+            
+        edge_positions = valid_edge_positions
+        #pygame.draw.circle(see_through2, GREEN_HIGHLIGHT, to_screen_coords((self.start_x,self.start_y)), (math.sqrt(5)+self.radius)/8*640,width=round(self.radius*640/8*2))
+        edge_position_angles=[]
+        for pos in edge_positions:
+            angle=math.radians(90)-math.atan2(pos[1]-self.start_y,pos[0]-self.start_x)
+            if angle<0:
+                angle=2*math.pi+angle
+            edge_position_angles.append(angle)
+            pygame.draw.circle(see_through, RED_HIGHLIGHT, to_screen_coords(pos), 0.35/8*640)
+
+        
+        #sort edge positions by angle
+        
+        edge_position_angles.sort()
+        draw_first_arc=True
+        for angle in pieces_in_range_angles:
+            if edge_position_angles[0]<angle<edge_position_angles[1]:
+                draw_first_arc=False
+
+        start = 0 if draw_first_arc else 1
+        
+        for i in range(start,len(edge_position_angles)-1,2):
+            arc(see_through, RED_HIGHLIGHT, to_screen_coords((self.start_x,self.start_y)),(math.sqrt(5)+self.radius)/8*640 ,edge_position_angles[i],edge_position_angles[i+1]-edge_position_angles[i],int(self.radius/8*640*2))
+
+
+
+        #arc(see_through2, GREEN_HIGHLIGHT, to_screen_coords((self.start_x,self.start_y)),(math.sqrt(5)+self.radius)/8*640 ,edge_position_angles[0],edge_position_angles[1]-edge_position_angles[0],int(self.radius/8*640*2))
+
+        #i dont know what im doing
+        if not draw_first_arc:
+            arc(see_through, RED_HIGHLIGHT, to_screen_coords((self.start_x,self.start_y)),(math.sqrt(5)+self.radius)/8*640 ,edge_position_angles[-1],math.radians(360)-edge_position_angles[-1],int(self.radius/8*640*2))
+            arc(see_through, RED_HIGHLIGHT, to_screen_coords((self.start_x,self.start_y)),(math.sqrt(5)+self.radius)/8*640 ,0,edge_position_angles[0],int(self.radius/8*640*2))
+            pygame.draw.circle(see_through, RED_HIGHLIGHT, to_screen_coords([self.start_x,self.start_y+Radius]), 0.35/8*640)
+        if len(edge_positions)==0:
+                pygame.draw.circle(see_through, RED_HIGHLIGHT, to_screen_coords((self.start_x,self.start_y)), (math.sqrt(5)+self.radius)/8*640,width=round(self.radius*640/8*2))
+
+        
 
     
 class Bishop(Piece):
@@ -756,6 +856,7 @@ class Bishop(Piece):
             
     def drag(self,new_p,pieces):
         if self.grabbed:
+            self.slide(0,0,pieces)
             path,dist=self.select_path((self.start_x,self.start_y),[[1,1],[-1,1]],new_p)
             path_len=math.sqrt(path[0]**2+path[1]**2)
             self.slide(path[0]*dist/path_len,path[1]*dist/path_len,pieces)
@@ -802,6 +903,7 @@ class Queen(Piece):
             
     def drag(self,new_p,pieces):
         if self.grabbed:
+            self.slide(0,0,pieces)
             path,dist=self.select_path((self.start_x,self.start_y),[[1,1],[-1,1],[1,0],[0,1]],new_p)
             path_len=math.sqrt(path[0]**2+path[1]**2)
             self.slide(path[0]*dist/path_len,path[1]*dist/path_len,pieces)
@@ -838,24 +940,125 @@ class King(Piece):
         fake_piece = King(self.start_x,self.start_y,self.color)
 
 
+        long_castle=True
+        short_castle=True
+        left_rook=None
+        right_rook=None
+
+        back_row=0.5
+        if self.color==black:
+            back_row=7.5
+                
+        for p in pieces:
+            if (isinstance(p,Rook) and p.color==self.color and p.turn==0):
+                if p.x<4:
+                    left_rook=p
+                else:
+                    right_rook=p
+                continue
+            if p==self:
+                continue
+            if abs(p.y-back_row)<self.radius*2:
+                if 0.5<p.x<4.5:
+                    long_castle=False
+                if 4.5<p.x<7.5:
+                    short_castle=False
+        if self.turn==0:
+            if long_castle:
+                if left_rook:
+                    if left_rook.turn==0:
+                        pygame.draw.circle(see_through2, GREEN_HIGHLIGHT, to_screen_coords((self.start_x-2,self.start_y)), self.radius/8*640)
+            if short_castle:
+                if right_rook:
+                    if right_rook.turn==0:
+                        pygame.draw.circle(see_through2, GREEN_HIGHLIGHT, to_screen_coords((self.start_x+2,self.start_y)), self.radius/8*640)
+
+        
+        if self.turn==0:
+            pieces=[p for p in pieces if (p!=left_rook and p!=right_rook)] 
+
         directions = [[1,1],[-1,-1],[1,-1],[-1,1],[0,1],[0,-1],[1,0],[-1,0]]
         end_positions = []
         for d in directions:
             fake_piece.slide(d[0],d[1],[p for p in pieces if p!=self],fake=True)
             end_positions.append((fake_piece.x,fake_piece.y))
             fake_piece.slide(0,0,[p for p in pieces if p!=self],fake=True)
-        
+
         for end_pos in end_positions:
             draw_line_round_corners_polygon(see_through2, to_screen_coords((self.start_x,self.start_y)), to_screen_coords(end_pos),GREEN_HIGHLIGHT, self.radius*2*640/8)
 
+        
 
-
+        
     def drag(self,new_p,pieces):
         if self.grabbed:
+
+            long_castle=True
+            short_castle=True
+            left_rook=None
+            right_rook=None
+
+            back_row=0.5
+            if self.color==black:
+                back_row=7.5
+            for p in pieces:
+                if (isinstance(p,Rook) and p.color==self.color and p.turn==0):
+                    if p.x<4:
+                        left_rook=p
+                    else:
+                        right_rook=p
+                    continue
+                if p==self:
+                    continue
+                if abs(p.y-back_row)<self.radius*2:
+                    if 0.5<p.x<4.5:
+                        long_castle=False
+                    if 4.5<p.x<7.5:
+                        short_castle=False
+            if left_rook:
+                left_rook.x=left_rook.start_x
+            if right_rook:
+                right_rook.x=right_rook.start_x
+                
+
+
+            
+            self.x=self.start_x
+            self.y=self.start_y
+            
             path,dist=self.select_path((self.start_x,self.start_y),[[1,1],[-1,1],[1,0],[0,1]],new_p)
             path_len=math.sqrt(path[0]**2+path[1]**2)
             self.slide(clamp(path[0]*dist/path_len,-1,1),clamp(path[1]*dist/path_len,-1,1),pieces)
+            
+            if self.turn==0:
+                if long_castle:
+                    if left_rook:
+                        if left_rook.turn==0:
+                            if new_p[0]<self.start_x-1.5:
+                                self.slide(0,0,pieces)
+                                self.slide(-2,0,pieces)
+                                left_rook.x=self.x+1
+                                
+                if short_castle:
+                    if right_rook:
+                        if right_rook.turn==0:
+                            if new_p[0]>self.start_x+1.5:
+                                self.slide(0,0,pieces)
+                                self.slide(2,0,pieces)
+                                right_rook.x=self.x-1
 
+
+    def confirm(self,pieces):
+        super().confirm(pieces)
+
+        #this is so any rooks moved by castling get updated correctly
+        for p in pieces:
+            if p.x!=p.start_x or p.y!=p.start_y:
+                p.start_x=p.x
+                p.start_y=p.y
+                p.turn+=1
+
+        
     def draw_paths(self,pieces):
 
         if self.targeted:
@@ -999,6 +1202,9 @@ while not done:
     for piece in pieces:
         piece.draw()
 
+    #draw grabbed piece last so it will show up on top
+    if grabbed_piece:
+        grabbed_piece.draw()
     
         
     pygame.display.flip()
